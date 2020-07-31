2b85094fdfe389b9c397aa14065fda5b0645f156
amitiwary999 <tiwarya9@gmail.com>
2020-07-31 23:50:55 +0530

    added about section

--- portfolio_react/portfolio/src/home/Home.js
+++ portfolio_react/portfolio/src/home/Home.js
@@ -11,9 +11,9 @@ function Home(props) {
             <div className="row">
               <img src={image} className="profile-image img-responsive" />
               <div className="col my-auto">
-                <h1 className="text-left">Amit Tiwary</h1>
+                <h1 className="text-md-left text-sm-center">Amit Tiwary</h1>
                 <div className="row">
-                  <div className="social-list">
+                  <div className="ml-md-2 mx-auto">
                     <a className="social p-1" href="https://twitter.com/Tiwarya9Amit">
                       <i className="fa fa-twitter"></i>
                     </a>
@@ -31,10 +31,28 @@ function Home(props) {
               </div>
             </div>
           </div>
-          <div className="col-4 text-center">
-            <img src={image} className="profile-image" />
-          </div>
         </div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card p-4">
+              <p className="font-weight-bold">About Me</p>
+              <p>I am an Android Developer and have been in the field since over two year and being an avid Open Source lover, the source code to most of my apps can be easily found on my GitHub.
+              Currently, I work at Joynt as Software Engineer.
+              I have done my B.Tech from MAKAUT, Kolkata majoring in Computer Science and Engineering.
+              <br />
+              <br />
+              Apart from Android, I've also wrote some python script mainly related to webscraping.
+When I am not coding, I love to listen music or sort stories and watching stand-up comedy show</p>
+            </div>
+          </div>
+          <div>
+            <div className="card md-3">
+
+            </div>
+          </div>
+
+        </div>
+
       </div>
 
     );
